a16be80ad1356e001b0582dc3d533ecd3870ec39
Nicholas Yang <ny585@nyu.edu>
2018-11-12 01:45:30 -0500

    automated some more stuff, split up ruby stuff into multiple files

---
+++ renderer.rb
@@ -0,0 +1,21 @@
+require 'erb'
+
+class Renderer
+  attr_reader :substitutions
+  
+  def generate_substitutions(courses)
+    substitutions = []
+    courses.each do |code, name|
+      substitutions.push("{ regex: /#{code.gsub(" ", "(&nbsp;| )*")}(?=(&nbsp;|  |-|<))/g, replacement: \"#{name} -- #{code} \" }")
+    end
+
+    @substitutions = substitutions.join(",\n")
+  end
+  
+  def render(courses)
+    generate_substitutions(courses)
+    template = File.open("chrome/index.js.erb", 'rb', &:read)
+    res = ERB.new(template).result(binding)
+    File.open("chrome/index.js", 'w') { |file| file.write(res) }
+  end
+end
--- scraper.rb
+++ scraper.rb
@@ -1,40 +1,38 @@
 require 'json'
 require 'fileutils'
+require_relative './renderer'
 
-def scrape_subject(subject, courses)
-  lines = File.readlines(subject).each do |line|
-    if line =~ /#{subject} [0-9]* - [^"]*(?="?<\/div>)/
-      code, name = $~[0].split(" - ")
-      puts "Uhoh, #{courses[code]} and #{name} conflict!" if courses.key?(code) && courses[code] != name
-      courses[code] = name
+class Scraper < Object
+  def scrape_subject(subject, courses)
+    lines = File.readlines(subject).each do |line|
+      if line =~ /#{subject} [0-9]* - [^"]*(?="?<\/div>)/
+        code, name = $~[0].split(" - ")
+        puts "Uhoh, #{courses[code]} and #{name} conflict!" if courses.key?(code) && courses[code] != name
+        courses[code] = name
+      end
     end
   end
-end
 
-def scrape
-  courses = {}
-  scrape_subject("CORE-UA", courses)
-  scrape_subject("CHEM-UA",  courses)
-  scrape_subject("CSCI-UA",  courses)
-  scrape_subject("MATH-UA",  courses)
-  scrape_subject("FREN-UA",  courses)
-  scrape_subject("AHSEM-UA",  courses)
-  courses
-end
-
-def write_json(courses)
-  File.open("courses.json", 'w') { |file| file.write(JSON.generate(courses)) }
-end
-
-def write_js(courses)
-  substitutions = []
-  courses.each do |code, name|
-    substitutions.push("{ regex: /#{code.gsub(" ", "(&nbsp;)")}/g, replacement: \"#{name}\" }")
+  def scrape(subjects)
+    courses = {}
+    subjects.each do |subject|
+      scrape_subject(subject, courses)
+    end
+    courses
   end
-  File.open("chrome/courses.js", 'w') { |file| file.write(substitutions.join("\n")) }
 end
+subjects = [
+  "CORE-UA",
+  "CHEM-UA", 
+  "CSCI-UA", 
+  "MATH-UA", 
+  "FREN-UA", 
+  "AHSEM-UA"
+]
 
-courses = scrape
-write_js(courses)
-write_json(courses)
+s = Scraper.new
+courses = s.scrape(subjects)
+
+renderer = Renderer.new
+renderer.render(courses)
 
